9cfc8a4a8bcac107f846c215ee910215186008ea
leandromelgarejo-ordno <leandro.s.melgarejo@gmail.com>
2025-10-01 14:06:05 -0300

    Update TimeOutAfternoon.php

--- admin/vue/TimeOutAfternoon.php
+++ admin/vue/TimeOutAfternoon.php
@@ -3,7 +3,7 @@
 require_once('../includes/script.php');  
 require_once('../session/Login.php'); 
 
-$set_timezone = date_default_timezone_set("Asia/Manila");
+$set_timezone = date_default_timezone_set("America/Asuncion");
 
  $model = new Dashboard();
  $session = new AdministratorSession();
@@ -51,7 +51,7 @@ $set_timezone = date_default_timezone_set("Asia/Manila");
     
             $insert = "UPDATE `attendance` SET `time_out_afternoon` = '$time_in' WHERE `employee_id` = '$employee_id' AND `date` = '$date';";
 
-            $query = mysqli_query($connection, $insert) or die(mysqli_error().$insert);
+            $query = mysqli_query($connection, $insert) or die(mysqli_error($connection).$insert);
 
           
             //number of hours in the morning
@@ -74,7 +74,7 @@ $set_timezone = date_default_timezone_set("Asia/Manila");
               }   
 
               $num_hr = "UPDATE `attendance` SET `num_hr_afternoon` = '$int' WHERE `employee_id` = '$employee_id' AND `date` = '$date'";
-              $update = mysqli_query($connection, $num_hr) or die(mysqli_error().$num_hr);
+              $update = mysqli_query($connection, $num_hr) or die(mysqli_error($connection).$num_hr);
 
 
  
